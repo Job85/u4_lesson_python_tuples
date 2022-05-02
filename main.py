7b16844d55607874d692290260488f99ee6f08e7
Job85 <julianjernigan@ymail.com>
2022-05-02 10:34:09 -0400

    completed lesson

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
+from operator import itemgetter
+
+
 def create_tuple(list_of_nums):
     # Create a tuple from a list and return it.
-    return
+    list = tuple(list_of_nums)
+    return list
 
 
 def get_value(tup):
     # Return the 3rd value in the provided tuple
-    return
+    return (tup[2])
 
 
 def get_values(tup):
     # return the values from index 1-3
     # 3 should be inclusive
-    return
+    return (tup[1:3])
 
 
 def get_max(tup):
     # return the largest number in the provided tuple
-    return
-
+    return max(tup)
